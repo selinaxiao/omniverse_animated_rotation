4a6bef4f7f18c03334458c9f88ac90deaf349663
selina <yx229@cornell.edu>
2022-06-21 12:10:34 -0400

    second commit

--- exts/my.animated.rotation/my/animated/rotation/extension.py
+++ exts/my.animated.rotation/my/animated/rotation/extension.py
@@ -2,7 +2,8 @@ import omni.ext
 import omni.ui as ui
 import omni.kit.commands
 from pxr import Gf, Sdf, Usd
-import weakref
+from omni.anim.curve_editor import SingletonCurveEditor
+import omni.kit.widget.timeline.scripts.ui_helpers
 # from omni.anim.curve_editor.curve_editor_view import CurveEditorView
 # from omni.anim.curve_editor import CurveEditorExtension
 
@@ -43,42 +44,33 @@ class MyExtension(omni.ext.IExt):
                 ui.Button("rotate start", clicked_fn=lambda: self.on_click())
 
     def on_click(self):
-        self.prev_prim_path = []
-        self.prev_path_rotation = []
-        for i in range(len(self.prev_prims)):
-            self.prev_prim_path.append(str(self.prev_prims[i].GetPrimPath()))
-            self.prev_path_rotation.append(str(self.prev_prims[i].GetPrimPath())+'animationData.xformOp:rotateXYZ:x')
-            self.prev_path_rotation.append(str(self.prev_prims[i].GetPrimPath())+'animationData.xformOp:rotateXYZ:x')
-
-        if (self.prev_num_of_intervals != 0):
-            omni.kit.commands.execute('ChangeSelectionCurveCommand',
-            new_times=[],
-            new_track_names=[],
-            new_track_users=[],
-            delete_times=[t for t in range(0, self.prev_interval*self.prev_num_of_intervals, self.prev_num_of_intervals)],
-            delete_track_names=[a for a in self.prev_path_rotation for n in range(self.prev_num_of_intervals)],
-            delete_track_users=[b for b in self.prev_prim_path for m in range(self.prev_num_of_intervals*2)],
-            singleton_curve_editor_wp=<weakref at 0x000001B7835969F8; to 'SingletonCurveEditor' at 0x000001ABCEF618C8>)
-
-        context = omni.usd.get_context()
-        stage = context.get_stage()
-        prims = [stage.GetPrimAtPath(m) for m in context.get_selection().get_selected_prim_paths()]
-        
+        self.clean_up()
+    
         time_interval = self.field1.model.get_value_as_int()
         number_of_interval = self.field2.model.get_value_as_int()
         h_angle = self.field3.model.get_value_as_float()
         v_angle = self.field4.model.get_value_as_float()
+
+        # print("hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii")
         
         self.prev_interval = time_interval
         self.prev_num_of_intervals = number_of_interval
+        prims = self.get_prims()
         if prims:
             print(h_angle, v_angle)
             for i in range(len(prims)):
                 current_prim = prims[i].GetAttribute('xformOp:rotateXYZ').Get()
+                omni.kit.commands.execute('ChangeProperty',
+                    prop_path=Sdf.Path(str(prims[i].GetPrimPath())+'.xformOp:rotateXYZ'),
+                    value=Gf.Vec3d(0.0,0.0,0.0),
+                    prev=current_prim)
+
+            omni.kit.commands.execute('SetAnimCurveKey', time=Usd.TimeCode(0),
+                paths=[str(prims[i].GetPrimPath())+'.xformOp:rotateXYZ|x', str(prims[i].GetPrimPath())+'.xformOp:rotateXYZ|y', str(prims[i].GetPrimPath())+'.xformOp:rotateXYZ|z'])
         
 
-            for j in range(number_of_interval):
-                time = (j+1) * time_interval
+            for j in range(number_of_interval+1):
+                time = (j+1)* time_interval
                 omni.kit.commands.execute('ChangeProperty',
                     prop_path=Sdf.Path(str(prims[i].GetPrimPath())+'.xformOp:rotateXYZ'),
                     value=Gf.Vec3d((current_prim[0]+h_angle*(j+1)), current_prim[1], (current_prim[2]+v_angle*(j+1))),
@@ -94,33 +86,78 @@ class MyExtension(omni.ext.IExt):
 
 
 
+    """helper functions"""
 
+    # remove all the key frames from previous start
+    def clean_up(self):
+        self.prev_prim_path = []
+        self.prev_path_rotation = []
+        self.prev_rotation = []
+        for i in range(len(self.prev_prims)):
+            self.prev_prim_path.append(str(self.prev_prims[i].GetPrimPath()))
+            self.prev_path_rotation.append(str(self.prev_prims[i].GetPrimPath())+'animationData.xformOp:rotateXYZ:x')
+            self.prev_path_rotation.append(str(self.prev_prims[i].GetPrimPath())+'animationData.xformOp:rotateXYZ:z')
+            self.prev_rotation.append((str(self.prev_prims[i].GetPrimPath())+'.xformOp:rotateXYZ:x'))
+            self.prev_rotation.append((str(self.prev_prims[i].GetPrimPath())+'.xformOp:rotateXYZ:y'))
+            self.prev_rotation.append((str(self.prev_prims[i].GetPrimPath())+'.xformOp:rotateXYZ:z'))
+        # print([t for t in range(0, self.prev_interval*self.prev_num_of_intervals, self.prev_num_of_intervals)])
+        print("-------------------------------")
+        print([a for a in self.prev_path_rotation for n in range(self.prev_num_of_intervals)])
+        print("-------------------------------")
+        print([b for b in self.prev_prim_path for m in range(self.prev_num_of_intervals*2)])
+        print("-------------------------------")
+        print(SingletonCurveEditor.get_instance_ref())
 
+        
+        context = omni.usd.get_context()
+        stage = context.get_stage()
 
-# curvelistview = CurveEditorView(self)._get_curve_list_view()
+        if (self.prev_num_of_intervals != 0):
+            delete_times=[t for t in range(self.prev_interval, self.prev_interval*self.prev_num_of_intervals+1, self.prev_interval)]
+            delete_track_names=[a for a in self.prev_path_rotation for n in range(self.prev_num_of_intervals)]
+            delete_track_users=[b for b in self.prev_prim_path for m in range(self.prev_num_of_intervals*2)]
+            
+            print("-------------------------------")
+            print(delete_times)
+            print("-------------------------------")
+            print(delete_track_users)
 
+            omni.kit.commands.execute('ChangeSelectionCurveCommand',
+            new_times=[],
+            new_track_names=[],
+            new_track_users=[],
+            delete_times=delete_times,
+            delete_track_names=delete_track_names,
+            delete_track_users=delete_track_users,
+            singleton_curve_editor_wp=SingletonCurveEditor.get_instance_ref(),
+            edit_scope=omni.kit.widget.timeline.scripts.ui_helpers.EditScope())
 
+            root_layer = stage.GetRootLayer()
+            session_layer = stage.GetSessionLayer()
+            print(root_layer)
 
-# curvelistview = CurveEditorView(self)._get_curve_list_view()
-# print(curvelistview._clean_all_curve_frame())
-# curves = curvelistview._curve_list
-# print(curves)
-# for curve in curves:
-#     curvelistview._on_curve_double_clicked(curve)
+            for keys in range(len(delete_times)):
+                print(delete_times[keys])
+                
+                omni.kit.commands.execute('RemoveAnimCurveKey',
+                    stage=Usd.Stage.Open(rootLayer=root_layer, sessionLayer=session_layer),
+                    paths=[self.prev_rotation[0]],
+                    time=Usd.TimeCode(delete_times[keys]))
 
-# print(curvelistview._get_multi_selected_keys())
+                
+                omni.kit.commands.execute('RemoveAnimCurveKey',
+                    stage=Usd.Stage.Open(rootLayer=root_layer, sessionLayer=session_layer),
+                    paths=[self.prev_rotation[2]],
+                    time=Usd.TimeCode(delete_times[keys]))
+                
+                omni.kit.commands.execute('RemoveAnimCurveKey',
+                    stage=Usd.Stage.Open(rootLayer=root_layer, sessionLayer=session_layer),
+                    paths=[self.prev_rotation[1]],
+                    time=Usd.TimeCode(delete_times[keys]))
 
-# curvelistview._command_delete_key()
+    def get_prims(self):
+        context = omni.usd.get_context()
+        stage = context.get_stage()
+        prims = [stage.GetPrimAtPath(m) for m in context.get_selection().get_selected_prim_paths()]
+        return prims
 
-# import omni.kit.commands
-# from omni import kit.widget.timeline.scripts.ui_helpers
-
-# omni.kit.commands.execute('ChangeSelectionCurveCommand',
-# 	new_times=[7.0, 4.0, 0.0, 8.0, 12.0],
-# 	new_track_names=['/World/Cube/animationData.xformOp:rotateXYZ:x', '/World/Cube/animationData.xformOp:rotateXYZ:x', '/World/Cube/animationData.xformOp:rotateXYZ:x', '/World/Cube/animationData.xformOp:rotateXYZ:x', '/World/Cube/animationData.xformOp:rotateXYZ:x'],
-# 	new_track_users=['/World/Cube', '/World/Cube', '/World/Cube', '/World/Cube', '/World/Cube'],
-# 	delete_times=[],
-# 	delete_track_names=[],
-# 	delete_track_users=[],
-# 	singleton_curve_editor_wp=<weakref at 0x000001B7835969F8; to 'SingletonCurveEditor' at 0x000001ABCEF618C8>,
-# 	edit_scope=<omni.kit.widget.timeline.scripts.ui_helpers.EditScope object at 0x000001AC066DDCC8>)
